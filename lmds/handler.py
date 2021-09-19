22857ebcf5c93986dfdfc57188411eec492486ea
Adolfo Diaz Taracena <adolfo_10_dt@hotmail.com>
2021-09-19 13:30:19 -0500

    new changes

--- lmds/handler.py
+++ lmds/handler.py
@@ -2,6 +2,9 @@ import json
 from aws_lambda_powertools.utilities.idempotency import (
     IdempotencyConfig, DynamoDBPersistenceLayer, idempotent
 )
+from aws_lambda_powertools import Logger 
+
+logger = Logger(service="Storitest")
 
 persistence_layer = DynamoDBPersistenceLayer(table_name="IdempotencyTable")
 
@@ -9,19 +12,24 @@ persistence_layer = DynamoDBPersistenceLayer(table_name="IdempotencyTable")
 # in the event json object as our payload
 config = IdempotencyConfig(event_key_jmespath="body")
 
-
-
-
+@logger.inject_lambda_context
 @idempotent(config=config, persistence_store=persistence_layer)
 def start(event, context):
-    body = json.loads(event['body'])
-    payment = create_subscription_payment(
-        user=body['user'],
-        product=body['product_id']
-    )
+    try :
+        logger.info("Starting the event...")
+        logger.info("The event: {}".format(event))
+        body = json.loads(event['body'])
 
-    return {
-        "payment_id": payment.id,
-        "message": "success",
-        "statusCode": 200
-    }
+        payment = create_subscription_payment(
+            user=body['user'],
+            product=body['product_id']
+        )
+
+        return {
+            "payment_id": payment.id,
+            "message": "success",
+            "statusCode": 200
+        }
+    except Exception as Error:
+        logger.error("Something wents wrong when trying to start the lambda:{}".format(Error))
+
