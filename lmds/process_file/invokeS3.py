93f86c1da11a1b0a81d177c7734d35952ceba744
Adolfo Diaz Taracena <adolfo_10_dt@hotmail.com>
2021-09-19 17:46:30 -0500

    probando

--- lmds/process_file/invokeS3.py
+++ lmds/process_file/invokeS3.py
@@ -2,7 +2,9 @@ from tools.tools_aws import descarga_de_s3 as downloadS3
 
 def start(event, context):
     try:
+        print("Probando evento......")
         print(event)
+
 
     except Exception as Error:
         print("Error as: {}".format())
